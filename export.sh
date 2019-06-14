5f9568143061cd0976e9953f09648cc328f85bee
Timóteo Ramos <timoteoramos@ymail.com>
2019-06-14 15:21:25 -0300

    Fix tmux colors from zsh ; Removed tmux user configuration

--- export.sh
+++ export.sh
@@ -3,7 +3,6 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)
 
 mkdir -p ~/.config/nvim
 
-cp $SCRIPT_ROOT/tmux/tmux.conf ~/.tmux.conf
 cp $SCRIPT_ROOT/vim/unix.vimrc ~/.vimrc
 cp $SCRIPT_ROOT/neovim/unix.ginit.vim ~/.config/nvim/ginit.vim
 cp $SCRIPT_ROOT/neovim/unix.init.vim ~/.config/nvim/init.vim
--- import.sh
+++ import.sh
@@ -3,7 +3,6 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)
 
 mkdir -p ~/.config/nvim
 
-[ -f ~/.tmux.conf ] && cp ~/.tmux.conf $SCRIPT_ROOT/tmux/tmux.conf
 [ -f ~/.vimrc ] && cp ~/.vimrc $SCRIPT_ROOT/vim/unix.vimrc
 [ -f ~/.Xresources ] && cp ~/.Xresources $SCRIPT_ROOT/xresources/Xresources
 [ -f ~/.config/nvim/ginit.vim ] && cp ~/.config/nvim/ginit.vim $SCRIPT_ROOT/neovim/unix.ginit.vim
