f8362d88ca208a227daf8f47ff70b74cf0c0448a
Timóteo Ramos <timoteoramos@ymail.com>
2019-07-24 18:21:47 -0300

    Script adjustment for i3

--- export.sh
+++ export.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)
 
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config/{i3,nvim}
 
+cp $SCRIPT_ROOT/i3wm/config ~/.config/i3/
 cp $SCRIPT_ROOT/vim/unix.vimrc ~/.vimrc
 cp $SCRIPT_ROOT/neovim/unix.ginit.vim ~/.config/nvim/ginit.vim
 cp $SCRIPT_ROOT/neovim/unix.init.vim ~/.config/nvim/init.vim
 cp $SCRIPT_ROOT/neovim/settings.vim ~/.config/nvim/
+cp $SCRIPT_ROOT/i3wm/i3status ~/.i3status.conf
 cp $SCRIPT_ROOT/xresources/Xresources ~/.Xresources
 cp $SCRIPT_ROOT/zsh/zshrc ~/.zshrc
--- import.sh
+++ import.sh
@@ -3,8 +3,10 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)
 
 mkdir -p ~/.config/nvim
 
+[ -f ~/.i3status.conf ] && cp ~/.i3status.conf $SCRIPT_ROOT/i3wm/i3status
 [ -f ~/.vimrc ] && cp ~/.vimrc $SCRIPT_ROOT/vim/unix.vimrc
 [ -f ~/.Xresources ] && cp ~/.Xresources $SCRIPT_ROOT/xresources/Xresources
+[ -f ~/.config/i3/config ] && cp ~/.config/i3/config $SCRIPT_ROOT/i3wm/
 [ -f ~/.config/nvim/ginit.vim ] && cp ~/.config/nvim/ginit.vim $SCRIPT_ROOT/neovim/unix.ginit.vim
 [ -f ~/.config/nvim/init.vim ] && cp ~/.config/nvim/init.vim $SCRIPT_ROOT/neovim/unix.init.vim
 [ -f ~/.config/nvim/settings.vim ] && cp ~/.config/nvim/settings.vim $SCRIPT_ROOT/neovim/
